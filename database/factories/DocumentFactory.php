08c48caba652999e31d51417859b866ea044e5f8
MesbaHossein2010 <s.mesbahossein@gmail.com>
2025-05-17 16:48:34 +0330

    Persian factory

--- database/factories/DocumentFactory.php
+++ database/factories/DocumentFactory.php
@@ -12,35 +12,45 @@ use Faker\Provider\fa_IR\Text;
 
 class DocumentFactory extends Factory
 {
+    protected $persianFaker;
+
+    public function __construct(...$args)
+    {
+        parent::__construct(...$args);
+
+        $this->persianFaker = \Faker\Factory::create('fa_IR');
+        $this->persianFaker->addProvider(new Person($this->persianFaker));
+        $this->persianFaker->addProvider(new Address($this->persianFaker));
+        $this->persianFaker->addProvider(new PhoneNumber($this->persianFaker));
+        $this->persianFaker->addProvider(new Company($this->persianFaker));
+        $this->persianFaker->addProvider(new Text($this->persianFaker));
+    }
+
     public function definition(): array
     {
-        $faker = \Faker\Factory::create('fa_IR');
-        $faker->addProvider(new Person($faker));
-        $faker->addProvider(new Address($faker));
-        $faker->addProvider(new PhoneNumber($faker));
-        $faker->addProvider(new Company($faker));
-        $faker->addProvider(new Text($faker));
+        $htmlSnippets = [
+            "<b>{$this->persianFaker->word}</b>",
+            '<br>',
+            '<br>',
+            "<i>{$this->persianFaker->word}</i>",
+            "<a href=\"{$this->faker->url}\">{$this->persianFaker->word}</a>",
+            "<code>{$this->faker->sentence}</code>",
+            "<pre><code>{$this->faker->paragraph}</code></pre>",
+            "<strong>{$this->persianFaker->sentence}</strong>",
+            "<em>{$this->persianFaker->sentence}</em>",
+            "<u>{$this->persianFaker->word}</u>",
+            "<span style=\"color:red\">{$this->persianFaker->word}</span>",
+        ];
 
-        // Generate rich Persian content
-        $persianParagraphs = [];
-        for ($i = 0; $i < rand(2, 5); $i++) {
-            $persianParagraphs[] = "<p>{$faker->realText(rand(200, 500))}</p>";
-        }
-
-        $htmlContent = implode("\n", [
-            "<h2>{$faker->sentence(4)}</h2>",
-            ...$persianParagraphs,
-            "<ul>" . implode('', array_map(fn() => "<li>{$faker->sentence(6)}</li>", range(1, rand(3, 7)))) . "</ul>",
-            "<blockquote>{$faker->sentence(10)}</blockquote>",
-            "<p>{$faker->realText(300)}</p>"
-        ]);
+        shuffle($htmlSnippets);
+        $htmlContent = implode(' ', $htmlSnippets);
 
         return [
-            'title' => $faker->sentence(3),
-            'slug' => $faker->unique()->slug(3),
+            'title' => $this->persianFaker->sentence(3),
+            'slug' => $this->faker->slug(),
             'content' => $htmlContent,
-            'section_id' => Section::inRandomOrder()->value('id') ?? Section::factory(),
-            'created_at' => $faker->dateTimeBetween('-1 year', 'now'),
+            'section_id' => $this->faker->numberBetween(1, Section::count()),
+            'created_at' => now(),
             'updated_at' => now(),
             'user_id' => 'admin'
         ];
