00cbfa9a0d22439b275a2191757d83fccc9b1db7
MesbaHossein2010 <s.mesbahossein@gmail.com>
2025-05-17 16:55:59 +0330

    Persian factory at last!(i think so)

--- database/factories/DocumentFactory.php
+++ database/factories/DocumentFactory.php
@@ -29,24 +29,24 @@ class DocumentFactory extends Factory
     public function definition(): array
     {
         $htmlSnippets = [
-            "<b>{$this->persianFaker->word}</b>",
+            "<b>" . $this->persianFaker->realText(10) . "</b>",
             '<br>',
             '<br>',
-            "<i>{$this->persianFaker->word}</i>",
-            "<a href=\"{$this->faker->url}\">{$this->persianFaker->word}</a>",
-            "<code>{$this->faker->sentence}</code>",
-            "<pre><code>{$this->faker->paragraph}</code></pre>",
-            "<strong>{$this->persianFaker->sentence}</strong>",
-            "<em>{$this->persianFaker->sentence}</em>",
-            "<u>{$this->persianFaker->word}</u>",
-            "<span style=\"color:red\">{$this->persianFaker->word}</span>",
+            "<i>" . $this->persianFaker->realText(10) . "</i>",
+            "<a href=\"{$this->faker->url}\">" . $this->persianFaker->realText(10) . "</a>",
+            "<code>{$this->faker->sentence}</code>",          // English inside code
+            "<pre><code>{$this->faker->paragraph}</code></pre>", // English inside pre/code
+            "<strong>" . $this->persianFaker->realText(20) . "</strong>",
+            "<em>" . $this->persianFaker->realText(20) . "</em>",
+            "<u>" . $this->persianFaker->realText(10) . "</u>",
+            "<span style=\"color:red\">" . $this->persianFaker->realText(10) . "</span>",
         ];
 
         shuffle($htmlSnippets);
         $htmlContent = implode(' ', $htmlSnippets);
 
         return [
-            'title' => $this->persianFaker->sentence(3),
+            'title' => $this->persianFaker->realText(30),
             'slug' => $this->faker->slug(),
             'content' => $htmlContent,
             'section_id' => $this->faker->numberBetween(1, Section::count()),
