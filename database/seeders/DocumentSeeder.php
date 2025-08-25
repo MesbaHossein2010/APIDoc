d631087166a96a42710164589e3a5daddd71b300
MesbaHossein2010 <s.mesbahossein@gmail.com>
2025-08-25 17:16:15 +0330

    env->gitignor

--- database/seeders/DocumentSeeder.php
+++ database/seeders/DocumentSeeder.php
@@ -16,7 +16,7 @@ class DocumentSeeder extends Seeder
     {
         // Create in smaller batches
         $batchSize = 50; // Process 50 records at a time
-        $totalRecords = 50;
+        $totalRecords = 500;
 
         for ($i = 0; $i < $totalRecords; $i += $batchSize) {
             Document::factory()
